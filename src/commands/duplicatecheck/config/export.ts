d2d89a38a8e135e9f326655aa3cef34a293472be
wojtek <tom@192.168.0.30>
2020-04-28 18:32:47 +0200

    export

--- src/commands/duplicatecheck/config/export.ts
+++ src/commands/duplicatecheck/config/export.ts
@@ -12,76 +12,84 @@ const messages = Messages.loadMessages('plauti-sfdx', 'export-config');
 
 export default class ExportConfig extends SfdxCommand {
 
-  public static description = messages.getMessage('commandDescription');
+    public static description = messages.getMessage('commandDescription');
 
-  public static examples = [
-  `$ sfdx plauti:duplicatecheck:config:export --targetusername myOrg@example.com
+    public static examples = [
+        `$ sfdx plauti:duplicatecheck:config:export --targetusername myOrg@example.com
   `
-  ];
+    ];
 
-//   public static args = [{name: 'file'}];
+    public static args = [{ name: 'file' }];
 
-//   protected static flagsConfig = {
-    // flag with a value (-n, --name=VALUE)
-        // name: flags.string({char: 'f', description: messages.getMessage('fileFlagDescription')}),
-    // force: flags.boolean({char: 'f', description: messages.getMessage('forceFlagDescription')})
-//   };
+    //   protected static flagsConfig = {
+    //     // flag with a value (-n, --name=VALUE)
+    //         name: flags.string({char: 'f', description: messages.getMessage('fileFlagDescription')}),
+    //         force: flags.boolean({char: 'd', description: messages.getMessage('directoryFlagDescription')})
+    //   };
 
-  // Comment this out if your command does not require an org username
-  protected static requiresUsername = true;
+    // Comment this out if your command does not require an org username
+    protected static requiresUsername = true;
 
-  // Comment this out if your command does not support a hub org username
-  protected static supportsDevhubUsername = true;
+    // Comment this out if your command does not support a hub org username
+    protected static supportsDevhubUsername = true;
 
-  // Set this to true if your command requires a project workspace; 'requiresProject' is false by default
-  protected static requiresProject = false;
+    // Set this to true if your command requires a project workspace; 'requiresProject' is false by default
+    protected static requiresProject = false;
 
-  protected static exportDir = './export/';
+    protected static defaultExportDirectory = './export/';
 
-  public async run(): Promise<AnyJson> {
+    public async run(): Promise<AnyJson> {
 
-    this.ux.startSpinner(`Downloading from export file`);
-    await fs.ensureDir(ExportConfig.exportDir);
-    
-    const conn = this.org.getConnection();
+        this.ux.startSpinner(`Downloading from export file`);
+        // await fs.ensureDir(ExportConfig.exportDir);
 
-    this.ux.log(`${conn.instanceUrl}/services/apexrest/dupcheck/dc3Api/search`);
-    this.ux.log(`Bearer ${conn.accessToken}`);
+        const conn = this.org.getConnection();
 
-    const searchInput = {
-        objectPrefix : "00Q",
-        objectData : {
-            firstName : 'woj'
+        this.ux.log(`${conn.instanceUrl}/services/apexrest/dupcheck/dc3Api/search`);
+
+        const searchInput = {
+            objectPrefix: "00Q",
+            objectData: {
+                firstName: 'woj'
+            }
         }
-    }
 
-    const defaultRequest = {
-        json: true,
-        headers: {
-            Authorization: `Bearer ${conn.accessToken}`
-        },
-        url : `${conn.instanceUrl}/services/apexrest/dupcheck/dc3Api/search`,
-        method : 'post',
-        body : JSON.stringify(searchInput)
-    };
-
-    const logger = this.ux;
-    let exportContent = null;
-    const targetFilename = ExportConfig.exportDir + new Date().toISOString + '.json';
-
-    await conn.requestRaw(defaultRequest)
-        .then(function (response) {
-            logger.log(JSON.stringify(response.body));
-            exportContent = response.body;
-        })
-        .catch(function (err) {
-            logger.errorJson(err);
-        });
-        
-        await fs.writeFile(`${targetFilename}`, exportContent);
-        this.ux.stopSpinner('Done!');
-        return { 
-            path: targetFilename 
+        const defaultRequest = {
+            json: true,
+            headers: {
+                Authorization: `Bearer ${conn.accessToken}`
+            },
+            url: `${conn.instanceUrl}/services/apexrest/dupcheck/dc3Api/search`,
+            method: 'post',
+            body: JSON.stringify(searchInput)
         };
-  }
+
+        const logger = this.ux;
+        let filePath = '';
+
+        if (this.args.file) {
+            filePath = this.args.file;
+        } else {
+            filePath = ExportConfig.defaultExportDirectory + (new Date().toISOString() + '.json');
+        }
+
+        logger.log(filePath);
+
+        let exportContent = null;
+        await conn.requestRaw(defaultRequest)
+            .then(function (response) {
+                logger.log(JSON.stringify(response.body));
+                exportContent = response.body;
+            })
+            .catch(function (err) {
+                logger.errorJson(err);
+            });
+
+        await fs.writeFile(`${filePath}`, exportContent);
+        this.ux.stopSpinner('Done!');
+
+        return {
+            path: filePath
+        };
+    }
 }
