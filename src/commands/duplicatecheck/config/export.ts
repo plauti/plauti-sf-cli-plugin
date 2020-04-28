293575d11ae4980596d15267be09e55016ee708a
wojtek <tom@192.168.0.30>
2020-04-28 14:43:54 +0200

    first command

--- src/commands/duplicatecheck/config/export.ts
+++ src/commands/duplicatecheck/config/export.ts
@@ -1,5 +1,5 @@
 import { flags, SfdxCommand } from '@salesforce/command';
-import { Messages, SfdxError } from '@salesforce/core';
+import { Messages, SfdxError, Org, Connection } from '@salesforce/core';
 import { AnyJson } from '@salesforce/ts-types';
 
 // Initialize Messages with the current plugin directory
@@ -36,49 +36,42 @@ export default class ExportConfig extends SfdxCommand {
   protected static requiresProject = false;
 
   public async run(): Promise<AnyJson> {
-    const name = this.flags.name || 'world';
 
     // this.org is guaranteed because requiresUsername=true, as opposed to supportsUsername
     const conn = this.org.getConnection();
-    const query = 'Select Name, TrialExpirationDate from Organization';
 
-    // The type we are querying for
-    interface Organization {
-      Name: string;
-      TrialExpirationDate: string;
+    this.ux.log(`${conn.instanceUrl}/services/apexrest/dupcheck/dc3Api/search`);
+    this.ux.log(`Bearer ${conn.accessToken}`);
+    
+    const searchInput = {
+        objectPrefix : "00Q",
+        objectData : {
+            firstName : 'woj'
+        }
     }
 
-    // Query the org
-    const result = await conn.query<Organization>(query);
+    const defaultRequest = {
+        json: true,
+        headers: {
+            Authorization: `Bearer ${conn.accessToken}`
+        },
+        url : `${conn.instanceUrl}/services/apexrest/dupcheck/dc3Api/search`,
+        method : 'post',
+        body : JSON.stringify(searchInput)
+    };
 
-    // Organization will always return one result, but this is an example of throwing an error
-    // The output and --json will automatically be handled for you.
-    if (!result.records || result.records.length <= 0) {
-      throw new SfdxError(messages.getMessage('errorNoOrgResults', [this.org.getOrgId()]));
-    }
+    const logger = this.ux;
+    let exportContent = null;
+    await conn.requestRaw(defaultRequest)
+        .then(function (response) {
+            logger.log('OK');
+            logger.log(JSON.stringify(response.body));
+            exportContent = response.body;
+        })
+        .catch(function (err) {
+            logger.errorJson(err);
+        });
 
-    // Organization always only returns one result
-    const orgName = result.records[0].Name;
-    const trialExpirationDate = result.records[0].TrialExpirationDate;
-
-    let outputString = `Hello ${name}! This is org: ${orgName}`;
-    if (trialExpirationDate) {
-      const date = new Date(trialExpirationDate).toDateString();
-      outputString = `${outputString} and I will be around until ${date}!`;
-    }
-    this.ux.log(outputString);
-
-    // this.hubOrg is NOT guaranteed because supportsHubOrgUsername=true, as opposed to requiresHubOrgUsername.
-    if (this.hubOrg) {
-      const hubOrgId = this.hubOrg.getOrgId();
-      this.ux.log(`My hub org id is: ${hubOrgId}`);
-    }
-
-    // if (this.flags.force && this.args.file) {
-    //   this.ux.log(`You input --force and a file: ${this.args.file}`);
-    // }
-
-    // Return an object to be displayed with --json
-    return { orgId: this.org.getOrgId(), outputString };
+        return { orgId: this.org.getOrgId(), exportContent};
   }
 }
